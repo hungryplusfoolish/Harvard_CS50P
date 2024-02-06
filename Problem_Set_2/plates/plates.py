a6d05cb187cd7bda0142633449322e0ce85151ad
hungryplusfoolish <christian.santos5176@gmail.com>
2024-02-06 11:12:24 -0500

    Add files via upload
    Problem set #2

---
+++ Problem_Set_2/plates/plates.py
@@ -0,0 +1,59 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(s):
+    if start(s) and length(s) and end(s) and punc(s):
+        return True
+    else:
+        return False
+
+def start(s):
+    for letter in s[:2]:
+        if letter.isalpha():
+            return True
+        else:
+            return False
+
+def length(s):
+    if 2 <= len(s) <= 6:
+        return True
+    else:
+        return False
+
+def end(s):
+    if s.isalpha():
+        return True
+
+    else:
+        list = []
+        nums_concat = ""
+        for letter in s:
+            list.append(letter)
+
+        for index in range(len(list)):
+            if list[index].isnumeric():
+                section = list[index:]
+                break
+
+        for nums in section:
+            nums_concat += nums
+
+        if nums_concat[0] != "0" and nums_concat.isnumeric():
+            return True
+        else:
+            return False
+
+
+def punc (s):
+    for char in s:
+        if char.isalpha() or char.isnumeric():
+            return True
+        else:
+            return False
+
+
+main()
